212bb3ad7f091ae94a9707823ef4b76a3349623c
ThebaultLouis <louis.thebault.ing@gmail.com>
2019-02-16 19:14:11 +0100

    Problem

---
+++ src/views/Problem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-container >
+      <v-layout row wrap mt-4>
+        <v-flex offset-xs3 xs6 class="product">
+          <v-card>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0 text-xs-center" >Vous vous êtes perdu sur notre site.</h3>
+                <h3 class="headline mb-0 text-xs-center" >Veuillez revenir à la page d'acceuil</h3>
+
+              </div>
+            <v-img
+              src="/Echarpe.png"
+            ></v-img>
+
+            </v-card-title>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
